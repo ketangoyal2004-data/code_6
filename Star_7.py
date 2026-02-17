56e5583b373e83437c8813b0068578cf31b0e25d
ketangoyal2004-data <ketangoyal858@gmail.com>
2026-02-17 16:29:12 +0530

    This is a branch goyal2 of Star_7.py

--- Star_7.py
+++ Star_7.py
@@ -11,7 +11,17 @@ class cal:
     def mul(a,b):
         return a * b
     
+    @staticmethod
+    def div(a,b):
+        return a / b
+    
+    @staticmethod
+    def exp(a,b):
+        return a ** b
+    
 obj = cal()
 print("a + b = ",obj.add(10,5))
 print("a - b = ",obj.sub(10,5)) 
 print("a * b = ",obj.mul(10,5)) 
+print("a / b =",obj.div(10,5))
+print("a ** b =",obj.exp(10,5))
