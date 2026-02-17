06e75d80ff0b1fb5095fbe3787da3a3909d2b6ad
ketangoyal2004-data <ketangoyal858@gmail.com>
2026-02-17 16:09:21 +0530

    This is a new file data enter

--- Star_2.py
+++ Star_2.py
@@ -3,3 +3,11 @@ for i in range(rows):
     for j in range(1, i + 1):
         print(j,end="")
     print("")
+
+class std:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+        print(f"Name: {self.name}, Age: {self.age}")
+
+obj = std("Ketan Goyal", 20)
--- Star_3.py
+++ Star_3.py
@@ -6,3 +6,12 @@ for i in range(rows,0,-1):
     for j in range(1,i + 1):
         print(b,end="")
     print("\r")
+
+
+class cal:
+    def __init__(self,a,b):
+        self.a = a
+        self.b = b
+        print("a + b = ",self.a + self.b)
+
+obj = cal(10,5)
