17675d9804e69ca9917cd45809fd790ac5c4d9dd
ketangoyal2004-data <ketangoyal858@gmail.com>
2026-02-17 16:18:36 +0530

    This is a goyal1 checkout branch

--- Star_6.py
+++ Star_6.py
@@ -4,3 +4,12 @@ for i in range(rows):
     for j in range(1, rows + 1):
         print(j,end="")
     print("")
+
+class std:
+    def __init__(self,name,course,branch):
+        self.name = name
+        self.course = course
+        self.branch = branch
+        print(f"Name:{self.name}\n Course:{self.course}\n Branch:{self.branch}")
+
+obj = std("Ketan Goyal","BCA","Computer Science")
