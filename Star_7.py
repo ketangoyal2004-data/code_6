a0ec4d6c24b8ca635e3359e686595b1f2aeda63f
ketangoyal2004-data <ketangoyal858@gmail.com>
2026-02-17 16:34:45 +0530

    This is a goyal2 a branch and files data modify

--- Star_7.py
+++ Star_7.py
@@ -19,9 +19,14 @@ class cal:
     def exp(a,b):
         return a ** b
     
+    @staticmethod
+    def mod(a,b):
+        return a % b
+    
 obj = cal()
 print("a + b = ",obj.add(10,5))
 print("a - b = ",obj.sub(10,5)) 
 print("a * b = ",obj.mul(10,5)) 
 print("a / b =",obj.div(10,5))
 print("a ** b =",obj.exp(10,5))
+print("a % b =",obj.mod(10,5))
