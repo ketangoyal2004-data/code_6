ee4faa5bdb937b4bdede4d5ec5dfcdb596c8a5f8
ketangoyal2004-data <ketangoyal858@gmail.com>
2026-02-17 15:41:43 +0530

    This is a files updates

--- Star_1.py
+++ Star_1.py
@@ -4,3 +4,14 @@ for i in range(rows):
     for j in range(i):
         print(i,end="")
     print("")
+
+class cal:
+    def __init__(self,a,b):
+        self.a = a
+        self.b = b
+        print("a + b = ",self.a + self.b)
+        print("a - b = ",self.a - self.b)
+        print("a * b = ",self.a * self.b)
+        print("a / b =",self.a / self.b)
+
+obj = cal(10,5)
